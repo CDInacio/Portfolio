370075a94ee4d75c481f53051a3a628b2dae5eff
claudiodantas <claudiodantas1996@gmail.com>
2023-04-09 11:47:13 -0300

    add new project

---
+++ src/constants/constants.js
@@ -0,0 +1,57 @@
+export const STACKS = [
+  { id: "stk1", stack: "HTML5" },
+  { id: "stk2", stack: "CSS3" },
+  { id: "stk3", stack: "JavaScript" },
+  { id: "stk4", stack: "Tailwind CSS" },
+  { id: "stk5", stack: "React" },
+  { id: "stk6", stack: "Next.js" },
+];
+
+export const LINKS = [
+  { text: "Home" },
+  { text: "Sobre" },
+  { text: "Projetos" },
+];
+
+export const PROJECTS = [
+  {
+    id: "p1",
+    name: "Weather App",
+    image: "/images/weather.png",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio cum corporis velit odit eos suscipit expedita facilis rerum culpa fugit.",
+    stacks: ["React", "TypeScript", "Tailwind CSS", "React Query"],
+  },
+  {
+    id: "p2",
+    name: "Bakery Site",
+    image: "/images/countries.png",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio cum corporis velit odit eos suscipit expedita facilis rerum culpa fugit.",
+    stacks: ["Next.js", "TypeScript", "Tailwind CSS"],
+  },
+  {
+    id: "p3",
+    name: "Countries",
+    image: "/images/countries.png",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio cum corporis velit odit eos suscipit expedita facilis rerum culpa fugit.",
+    stacks: ["React", "TypeScript", "Tailwind CSS"],
+  },
+  {
+    id: "p4",
+    name: "Movie and Series Finder",
+    image: "/images/movieFinder.png",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio cum corporis velit odit eos suscipit expedita facilis rerum culpa fugit.",
+    stacks: ["React", "Tailwind CSS"],
+  },
+  {
+    id: "p5",
+    name: "Know Your Calores",
+    image: "/images/calories.png",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio cum corporis velit odit eos suscipit expedita facilis rerum culpa fugit.",
+    stacks: ["React Native", "Tailwind CSS"],
+  },
+];
