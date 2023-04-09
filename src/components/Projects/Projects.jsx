d3b2f9f3e9be0e493de1533a462dfefdfb6f40a9
claudiodantas <claudiodantas1996@gmail.com>
2023-04-09 11:51:07 -0300

    fix import

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
-import { PROJECTS } from "../../app/constants/constants";
+import { PROJECTS } from "../../constants/constants";
 
 import ProjectItem from "./ProjectItem";
 
