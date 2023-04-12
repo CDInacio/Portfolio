c8a7ecd0e5fba2a795470ec933792377eb9fe6cf
claudiodantas <claudiodantas1996@gmail.com>
2023-04-12 20:41:55 -0300

    change primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
           "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
       },
       colors: {
-        primary: "#025A97",
+        primary: "#105196",
         secondary: "#333A96",
         background: "hsl(0, 0%, 98%)",
         panel: "hsl(0, 0%, 100%)",
