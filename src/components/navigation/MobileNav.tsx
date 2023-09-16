83965fcfacf036e69456be3f5499f41868f7aec4
claudiodantas <claudiodantas1996@gmail.com>
2023-09-16 20:27:19 -0300

    add links path and dark theme icons

--- src/components/navigation/MobileNav.tsx
+++ src/components/navigation/MobileNav.tsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+
+import { useTheme } from "next-themes";
+import Link from "next/link";
 
 import { LINKS } from "../../data/constants";
 
@@ -11,6 +15,8 @@ export default function MobileNav() {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
+  const { setTheme } = useTheme();
 
   return (
     <div className="flex md:hidden ">
@@ -47,21 +53,32 @@ export default function MobileNav() {
       ></div>
 
       <nav
-        className={`fixed top-0 left-0 w-full h-[50vh] bg-primary z-[150] transition-transform transform ${
+        className={`fixed top-0 left-0 w-full h-[50vh] bg-primary overflow-hidden z-[150] transition-transform transform ${
           showMenu ? "translate-y-0" : "-translate-y-full"
         }`}
         style={{ overflowY: "auto" }}
       >
         <ul className="flex flex-col items-center justify-center h-full text-white">
           {LINKS.map((link, i) => (
-            <li
-              key={i}
-              className="my-[10px] border-2 border-white px-[15px] rounded py-[5px]"
-            >
-              {link.text}
-            </li>
+            <Link href={link.path} key={i}>
+              <li className="my-[10px] border-2 border-white px-[15px] rounded py-[5px]">
+                {link.text}
+              </li>
+            </Link>
           ))}
         </ul>
+        <div className="absolute bottom-[20px] right-0 left-0 mx-auto w-fit  flex">
+          <BsFillMoonFill
+            size={20}
+            className={`cursor-pointer dark:text-white dark:opacity-100 opacity-20 `}
+            onClick={() => setTheme("dark")}
+          />
+          <BsFillSunFill
+            size={20}
+            className={`ml-[10px] dark:opacity-20 text-white  cursor-pointer`}
+            onClick={() => setTheme("light")}
+          />
+        </div>
       </nav>
     </div>
   );
