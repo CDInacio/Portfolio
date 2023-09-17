70bc952cc17ec33e38e630457a894f96df104d68
claudiodantas <claudiodantas1996@gmail.com>
2023-09-16 21:06:18 -0300

    fix divider width

--- src/components/Devider.tsx
+++ src/components/Devider.tsx
@@ -1,5 +1,7 @@
 export default function Devider() {
   return (
-    <div className="border-b-[1px] border-neutral-400 w-[800px] mx-auto"></div>
+    <div className="container">
+      <div className="border-b-[1px] border-neutral-300 w-[90%] md:w-[80%] mx-auto "></div>
+    </div>
   );
 }
