3e58cc55ce8c56639f5e3f4e96269598edbf4127
claudiodantas <claudiodantas1996@gmail.com>
2023-09-28 09:53:48 -0300

    remove  components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import Contact from "../components/Contact/Contact";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
-import Devider from "../components/ui/Devider";
 
 export default function Home() {
   // const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -16,14 +15,10 @@ export default function Home() {
 
   return (
     <>
-      <main className="z-10 mb-[50px]">
-        <div className="container  px-[20px] mx-auto overflow-x-hidden mb-[100px] sm:mb-0">
-          <Hero />
-          <Devider />
-          <Projects />
-          <Devider />
-          <Contact />
-        </div>
+      <main className="z-10  overflow-x-hidden">
+        <Hero />
+        <Projects />
+        <Contact />
       </main>
     </>
   );
