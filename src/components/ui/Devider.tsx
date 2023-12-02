b2ba05ec8d1aa88cd33aff418cf302264404b41a
claudiodantas <claudiodantas1996@gmail.com>
2023-12-01 21:47:09 -0300

    delete file

--- src/components/ui/Devider.tsx
+++
@@ -1,7 +0,0 @@
-export default function Devider() {
-  return (
-    <div className="container">
-      <div className="border-b-[1px] border-neutral-300 w-[90%] md:w-[80%] mx-auto "></div>
-    </div>
-  );
-}
