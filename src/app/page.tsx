d004ae120ecc84f108525739a4359bd504df48d3
claudiodantas <claudiodantas1996@gmail.com>
2023-12-01 21:46:57 -0300

    delete file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import Contact from "../components/Contact/Contact";
-import Hero from "../components/Hero/Hero";
-import Projects from "../components/Projects/Projects";
-
 export default function Home() {
   // const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -15,11 +11,7 @@ export default function Home() {
 
   return (
     <>
-      <main className="z-10  overflow-x-hidden">
-        <Hero />
-        <Projects />
-        <Contact />
-      </main>
+      <main className="z-10  overflow-x-hidden"></main>
     </>
   );
 }
