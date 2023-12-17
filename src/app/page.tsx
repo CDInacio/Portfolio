3b7a9733fe9e7ece02f8406ca9276c06952bf240
claudiodantas <claudiodantas1996@gmail.com>
2023-12-17 11:46:14 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,21 @@
-"use client";
-
-import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
-import { Element } from "react-scroll";
-
-import Image from "next/image";
-
-import Arrow from "./components/arrow/Arrow";
-import Navbar from "./components/navigation/Navbar";
+import About from "../components/about/About";
+import Contact from "../components/contact/Contact";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import Navbar from "../components/navigation/Navbar";
+import Projects from "../components/projects/Projects";
 
 export default function Home() {
   return (
-    <>
-      <main className="overflow-x-hidden snap-y snap-mandatory">
-        <header className="w-screen h-screen relative">
-          <Image
-            src="/images/hero.jpg"
-            className="object-cover"
-            fill
-            alt="imagem de cabeçalho"
-          />
-          <Navbar />
-          <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 text-white  text-center">
-            <h1 className="font-roboto text-8xl font-bold">CLÁUDIO DANTAS</h1>
-            <p className="text-xl font-robotoMono">
-              ESTUDANTE & PROGRAMADOR WEB
-            </p>
-            <div className="flex justify-center mt-2">
-              <a
-                href="https://www.linkedin.com/in/cl%C3%A1udio-dantas-520a1615b/"
-                target="_blank"
-              >
-                <IoLogoLinkedin size={20} className="mx-1 hover:text-sky-600" />
-              </a>
-              <a href="https://github.com/CDInacio" target="_blank">
-                <IoLogoGithub size={20} className="mx-1" />
-              </a>
-            </div>
-          </div>
-          <Arrow />
-        </header>
-        <section className="w-screen h-screen bg-backgroundDark shadow-custom-inset">
-          <Element name="sobre()"></Element>
-        </section>
-        <section className="w-screen h-screen bg-sky-800">
-          <Element name="projetos()"></Element>
-        </section>
+    <div className="overflow-x-hidden">
+      <main className="bg-bgDark">
+        <Navbar />
+        <Header id="Inicio" />
+        <About id="Sobre" />
+        <Projects id="Projetos" />
+        <Contact id="Contato" />
+        <Footer />
       </main>
-    </>
+    </div>
   );
 }
---
+++ src/components/about/About.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useRef } from "react";
+
+import Image from "next/image";
+
+import { useSection } from "../../context/activeSection";
+import { techIcons } from "../../data/constants";
+import useObserver from "../../hooks/useObserver";
+import Reveal from "../commom/Reveal";
+import Icon from "../ui/icon";
+
+const About = ({ id }: { id: string }) => {
+  const { setId } = useSection();
+
+  const ref = useRef(null);
+
+  useObserver(ref, setId, id);
+
+  return (
+    <section
+      ref={ref}
+      id={id}
+      className="py-20 lg:py-32 w-screen min-h-screen bg-bgDark container mx-auto text-white font-poppins px-5"
+    >
+      <Reveal>
+        <div className="flex items-center ">
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl  font-bold">
+            Sobre{" "}
+            <span className="text-primary">
+              mim<span>.</span>
+            </span>
+          </h1>
+          <p className="hidden sm:flex sm:w-[300px] lg:w-[700px] h-[1px] bg-neutral-600 ml-3 "></p>
+        </div>
+      </Reveal>
+      <Reveal>
+        <div className="flex flex-col lg:flex-row items-center  mt-20">
+          <div className="h-[200px] w-[200px] lg:min-w-[450px] lg:h-[250px]  relative">
+            <Image
+              src="/images/about.jpg"
+              fill
+              alt="about image"
+              className="rounded-full lg:rounded"
+            />
+          </div>
+          <p className="text-neutral-400 max-w-[800px] lg:ml-10 mt-10 lg:mt-0 text-sm sm:text-base  text-justify font-lato font-medium">
+            Olá, meu nome é Cláudio Vitor Dantas e sou natural de Serrinha, BA,
+            mas atualmente, resido em João Monlevade, MG. Estou seguindo minha
+            paixão pela tecnologia enquanto curso Sistemas de Informação na
+            Universidade Federal de Ouro Preto, campus João Monlevade. Há
+            aproximadamente um ano e meio, mergulhei de cabeça no mundo da
+            programação web, e desde então, venho aprimorando minhas habilidades
+            e conhecimentos nessa área. A programação web se tornou mais do que
+            apenas uma disciplina para mim; é uma verdadeira paixão que pretendo
+            transformar em uma carreira sólida e gratificante.
+          </p>
+        </div>
+      </Reveal>
+      <div className=" flex flex-col items-center mt-48">
+        <Reveal>
+          <div className=" flex flex-col items-center">
+            <h2 className="text-xl lg:text-3xl font-poppins font-bold">
+              Tecnologias que uso em{" "}
+              <span className="text-primary">
+                meus projetos<span>.</span>
+              </span>
+            </h2>
+            <div className=" flex justify-center flex-wrap max-w-[500px] gap-10 mt-20">
+              {techIcons.map((tech, index) => (
+                <Icon
+                  key={index}
+                  iconType={tech.text}
+                  className="w-[40px] sm:w-[60px] h-[40px] sm:h-[60px]"
+                />
+              ))}
+            </div>
+          </div>
+        </Reveal>
+      </div>
+    </section>
+  );
+};
+
+export default About;
