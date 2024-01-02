c65ced451ba6d9345d41afbaa81338a57cda0d81
claudiodantas <claudiodantas1996@gmail.com>
2024-01-02 09:55:45 -0300

    feat: forwardRef  input component

--- src/components/input/TextInput.tsx
+++ src/components/input/TextInput.tsx
@@ -1,19 +1,25 @@
+import React from "react";
+
 import { twMerge } from "tailwind-merge";
 
 interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
+  ref?: React.Ref<HTMLInputElement>;
 }
 
-const TextInput = ({ className, ...props }: TextInputProps) => {
-  return (
-    <input
-      {...props}
-      className={twMerge(
-        "bg-neutral-800 py-2 px-3  rounded text-sm md:text-base border-[1px] border-transparent focus:border-[1px] focus:border-primary transtition duration-200",
-        className
-      )}
-    />
-  );
-};
+const TextInput = React.forwardRef<HTMLInputElement, TextInputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={twMerge(
+          "bg-neutral-800 w-full mb-4  py-2 px-3  rounded border-[1px] border-transparent focus:border-[1px] focus:border-primary transtition duration-200",
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
 
 export default TextInput;
